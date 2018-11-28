c7fe3407d0429c51ef60b25bac6cf600e6059f53
julien.feuillad <julien.feuillade@he-arc.ch>
2018-11-28 18:09:09 +0100

    Repair my fault from the last commit

--- resources/views/product/listProduct.blade.php
+++ resources/views/product/listProduct.blade.php
@@ -4,7 +4,6 @@
 <div class="container">
     <h1> Product List</h1>
 
-<<<<<<< HEAD
         <button class="btn btn-info" onclick="location.href='{{route('addProduct')}}'">Add Product</button>
         @if(!$products->isEmpty())
         <table class="table table-hover">
@@ -20,29 +19,6 @@
                 <td class="clickable align-middle" href="#" data-id="{{ $product->id }}">{{ $product->name}}</td>
                 <td class="clickable align-middle" href="#" data-id="{{ $product->id }}">{{ $product->quantity}}</td>
                 <td class="buttons align-middle text-justify">
-=======
-    <button class="btn btn-info" onclick="location.href='{{route('addProduct')}}'">Add Product</button>
-
-    <table class="table table-hover">
-        <tr>
-            <th>
-                @lang('Image')</th>
-            <th>
-                @lang('Name')</th>
-            <th>
-                @lang('Quantity')</th>
-            <th>
-                @lang('Action')</th>
-        </tr>
-        @foreach($products as $product)
-
-        <tr onclick="location.href='{{route('editProduct', ['id' => $product->id])}}'">
-            <td class="clickable align-middle" href="#" data-id="{{ $product->id }}">{{ $product->image}}</td>
-            <td class="clickable align-middle" href="#" data-id="{{ $product->id }}">{{ $product->name}}</td>
-            <td class="clickable align-middle" href="#" data-id="{{ $product->id }}">{{ $product->quantity}}</td>
-            <td class="buttons align-middle text-justify">
-
-
                 <form action="{{ route('updateProduct') }}" method="POST">
                     @csrf
                     <input type='hidden' value='{{$product->id}}' name='product_id'>
