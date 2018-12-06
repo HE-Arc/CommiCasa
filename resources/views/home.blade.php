e36cca02da891c5e9b8eaeac29a45ee10fbb8d5f
Srdjenovic Luca <luca.srdjenov@he-arc.ch>
2018-12-06 16:29:38 +0100

    Initalize home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
+
+        <div class="container">
+            <h1 class="">Bienvenu sur CommiCasa</h1>
+            <p>Apprenez à ne plus oublier ce que vous devez acheter dans votre maison</p>
+
+            <button class="btn btn-info" onclick="window.location.href='{{route('listProduct')}}'">Créer votre propre liste de course</button>
+            <br>
+            <br>
+            <button class="btn btn-info" onclick="window.location.href='{{route('listRecipe')}}'">Accèder à des recettes</button>
+        </div>
+
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -16,8 +27,8 @@
 
                     You are logged in!
                 </div>
-            </div>
         </div>
+
     </div>
 </div>
 @endsection
