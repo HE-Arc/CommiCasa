efe46a00a60afaf15ab9a4ef5bf97072b1f2bc81
Srdjenovic Luca <luca.srdjenov@he-arc.ch>
2018-12-06 17:00:09 +0100

    Improve home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,16 +4,41 @@
 <div class="container">
     <div class="row justify-content-center">
 
-        <div class="container">
-            <h1 class="">Bienvenu sur CommiCasa</h1>
-            <p>Apprenez à ne plus oublier ce que vous devez acheter dans votre maison</p>
+        <div class="jumbotron">
+            <div class="container">
 
-            <button class="btn btn-info" onclick="window.location.href='{{route('listProduct')}}'">Créer votre propre liste de course</button>
-            <br>
-            <br>
-            <button class="btn btn-info" onclick="window.location.href='{{route('listRecipe')}}'">Accèder à des recettes</button>
+            </div>
+            <h1 class="display-3">Hello <strong>{{Auth::user()->name}}</strong>, bienvenu sur CommiCasa </h1>
+            <p>Apprenez à ne plus oublier ce que vous devez acheter dans votre maison</p>
         </div>
 
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2>Votre liste de produit</h2>
+                    <p>Découvrez votre liste de course</p>
+                    <p><button class="btn btn-secondary" onclick="window.location.href='{{route('listProduct')}}'">Créer votre propre liste de course</button></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Votre liste de Shopping</h2>
+                    <p>Découvrez votre liste de shopping</p>
+                    <p><button class="btn btn-secondary" onclick="window.location.href='{{route('listRecipe')}}'">Créer votre propre liste de course</button></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Votre liste de produit</h2>
+                    <p>Découvrez vos recettes</p>
+                    <p><button class="btn btn-secondary" onclick="window.location.href='{{route('listShopping')}}'">Créer votre propre liste de course</button></p>
+                </div>
+                <div class="col-md-4">
+                    <h2>Votre liste de catégories</h2>
+                    <p>Découvrez votre catérogies</p>
+                    <p><button class="btn btn-secondary" onclick="window.location.href='{{route('listCategory')}}'">Créer votre propre liste de course</button></p>
+                </div>
+            </div>
+        </div>
+
+
+        <!--
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Dashboard</div>
@@ -27,6 +52,7 @@
 
                     You are logged in!
                 </div>
+                -->
         </div>
 
     </div>
